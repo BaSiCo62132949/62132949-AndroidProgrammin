026f54fdad7cd052fe9478a45c1657afd57a5064
BaSiCo62132949 <co.bs.62cntt@ntu.edu.vn>
2025-03-07 15:06:12 +0700

    java Màn hình

--- Ex6_IntentDonGian/app/src/main/java/ntu/basico/ex6_intentdongian/MainActivity.java
+++ Ex6_IntentDonGian/app/src/main/java/ntu/basico/ex6_intentdongian/MainActivity.java
@@ -1,24 +1,44 @@
 package ntu.basico.ex6_intentdongian;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button nutMH2;
+    Button nutMH3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        nutMH2 = (Button) findViewById(R.id.btnMH2);
+        nutMH3 = (Button) findViewById(R.id.btnMH3);
+
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Xử lý chuyển màn hình
+                // B1. Tạo một Intent
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
+                // B2. Gửi
+                startActivity(intentMH2);
+            }
         });
+
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH3 = new Intent(MainActivity.this, MH3Activity.class);
+                startActivity(intentMH3);
+            }
+        });
+
     }
 }
